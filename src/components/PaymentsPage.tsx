53ad70405b06b69055d3ad5684795698a39b458f
Chris Meade <chrism@target-technology.co.uk>
2026-03-08 15:25:55 +0000

    moved table rendering out of page component, added filters state, clear filter button and error component.

--- src/components/PaymentsPage.tsx
+++ src/components/PaymentsPage.tsx
@@ -1,66 +1,88 @@
 import {
+  ClearButton,
   Container,
-  Table,
-  TableBodyWrapper,
-  TableHeader,
-  TableHeaderRow,
-  TableHeaderWrapper,
-  TableRow,
-  TableWrapper,
+  FilterRow,
+  SearchButton,
+  SearchInput,
   Title,
 } from './components'
 import { I18N } from '../constants/i18n'
-import { useFetchTableData } from '../hooks/useFetchTableData'
-import { Payment } from '../types/payment'
-import { TableCellResolver } from './CellRenderer'
+import {
+  SearchParams,
+  SearchParamsSchema,
+  useFetchTableData,
+} from '../hooks/useFetchTableData'
+import { FormEvent, useState } from 'react'
+import { DataTable } from './DataTable'
+import { ErrorAlert } from './ErrorAlert'
 
 // rename easier to type
 const labels = I18N
 
-// retrieve the headers from the contants
-const tableHeaders = Object.entries(labels)
-  .filter(([k]) => k.includes('TABLE_HEADER'))
-  .map(([, v]) => v)
-
-const displayColumns: (keyof Payment)[] = [
-  'id',
-  'date',
-  'amount',
-  'customerName',
-  'currency',
-  'status',
-]
+const defaultFilters: SearchParams = {
+  search: '',
+  page: 1,
+  pageSize: 5,
+  currency: '',
+}
 
 export const PaymentsPage = () => {
-  const { data: paymentResponse } = useFetchTableData({
-    search: '',
-    page: 1,
-    pageSize: 5,
-  })
+  const [inputValue, setInputValue] = useState('')
+  const [filters, setFilters] = useState<SearchParams>(defaultFilters)
+
+  const hasChanges = inputValue !== defaultFilters.search //Todo extend to other filters in next steps
+
+  const { data: paymentResponse, error } = useFetchTableData(filters)
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    if (!hasChanges) return
+    // zod validation here: single source of truth, fail gracefully, no malformed requests
+    const result = SearchParamsSchema.safeParse({
+      ...filters,
+      search: inputValue,
+    })
+
+    if (result.success) {
+      setFilters(result.data)
+    }
+    // todo error handling
+  }
+
+  const handleClearFilters = () => {
+    setFilters(defaultFilters)
+    setInputValue('')
+  }
 
   return (
     <Container>
       <Title>{labels.PAGE_TITLE}</Title>
-      <TableWrapper>
-        <Table>
-          <TableHeaderWrapper>
-            <TableHeaderRow>
-              {tableHeaders.map((s) => (
-                <TableHeader>{s}</TableHeader>
-              ))}
-            </TableHeaderRow>
-          </TableHeaderWrapper>
-          <TableBodyWrapper>
-            {paymentResponse?.payments.map((p: Payment) => (
-              <TableRow key={p.id}>
-                {displayColumns.map((col) => (
-                  <TableCellResolver key={col} type={col} value={p[col]} />
-                ))}
-              </TableRow>
-            ))}
-          </TableBodyWrapper>
-        </Table>
-      </TableWrapper>
+      <form onSubmit={handleSubmit} role="search">
+        <FilterRow>
+          <SearchInput
+            value={inputValue}
+            placeholder={labels.SEARCH_PLACEHOLDER}
+            aria-label={labels.SEARCH_LABEL}
+            type="search"
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+          />
+          <SearchButton type="submit" disabled={!hasChanges}>
+            {labels.SEARCH_BUTTON}
+          </SearchButton>
+          {hasChanges && (
+            <ClearButton type="button" onClick={handleClearFilters}>
+              {labels.CLEAR_FILTERS}
+            </ClearButton>
+          )}
+        </FilterRow>
+      </form>
+
+      {error ? (
+        <ErrorAlert error={error} />
+      ) : (
+        // default to an empty array for now need to implement proper laoding states
+        <DataTable payments={paymentResponse?.payments ?? []} />
+      )}
     </Container>
   )
 }

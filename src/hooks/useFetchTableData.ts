654b35bc73e2fc7d03cd990b7c4a7450bc89fe11
Chris Meade <chrism@target-technology.co.uk>
2026-03-08 14:26:56 +0000

    fetch payments data hook

---
+++ src/hooks/useFetchTableData.ts
@@ -0,0 +1,34 @@
+import z from 'zod'
+import { CURRENCIES } from '../constants'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { PaymentSearchResponse } from '../types/payment'
+
+const STALE_TIME = 5000 // 5 seconds
+const QUERY_KEY = 'payments'
+const BASE_URL = '/api/payments'
+
+export const SearchParamsSchema = z.object({
+  search: z.string(),
+  currency: z.enum(CURRENCIES).optional(),
+  page: z.number(),
+  pageSize: z.number(),
+})
+
+type SearchParams = z.infer<typeof SearchParamsSchema>
+
+const fetchPaymentsFromAPI = async (
+  searchParams: SearchParams,
+): Promise<PaymentSearchResponse> => {
+  // axios in the deps, handles serialisation of params and requests, validation will happen before we hit this function
+  const { data } = await axios.get(BASE_URL, { params: searchParams })
+  return data
+}
+
+export const useFetchTableData = (searchParams: SearchParams) => {
+  return useQuery({
+    queryKey: [QUERY_KEY, searchParams],
+    queryFn: () => fetchPaymentsFromAPI(searchParams),
+    staleTime: STALE_TIME,
+  })
+}
